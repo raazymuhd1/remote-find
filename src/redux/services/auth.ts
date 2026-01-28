9b57d2ad85d52271c6536f874f78400b4ccae5b2
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-28 23:23:02 +0700

    completed authApi for redux

--- src/redux/services/auth.ts
+++ src/redux/services/auth.ts
@@ -1,14 +1,34 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { User } from "../../types";
 
-const baseUrl = `http://localhost:5000/api/`
+interface UserAuthData {
+    username: string;
+    email: string;
+    password: string;
+}
 
-const authApi = createApi({
+const baseUrl = `http://localhost:5000/api/auth`
+
+export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        signin:  builder.mutation<User, string>({
-            query: () => ``
+        signin:  builder.mutation<User, Omit<UserAuthData, "username">>({
+            query: (userData) => ({
+                url: "/signin",
+                method: "POST",
+                body: userData
+            })
+        }),
+        signup: builder.mutation<User, UserAuthData>({
+            query: (userData) => ({
+                url: "/signup",
+                method: "POST",
+                body: userData
+            }),
         })
     })
 })
+
+
+export const { useSigninMutation, useSignupMutation } = authApi
