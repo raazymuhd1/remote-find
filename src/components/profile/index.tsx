3c117145fdfa32459fd4cd60b9bc37a1cb266187
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-29 08:52:42 +0700

    completed profile saved & applied jobs

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -20,9 +20,9 @@ const Profile = () => {
 
   return (
     <section 
-        className='md:w-[80%] w-full grid md:grid-cols-2 grid-cols-1 mx-auto p-[2rem] '>
+        className='md:w-[80%] h-[90vh] w-full flex md:flex-row flex-col gap-[2rem] mx-auto p-[2rem] '>
         {/* profile sidebar */}
-        <aside className="flex w-full md:w-[35%] p-[.8rem] gap-[1rem] flex-col sticky top-0 left-0">
+        <aside className="flex w-full md:w-[30%] p-[.8rem] bg-(--white) rounded-[15px] gap-[1rem] flex-col sticky top-0 left-0 hover:border-(--light-green) border-[1px] border-(--white) transition-[border,1s,ease-in-out]">
            <article className="w-full flex flex-col gap-[1rem]">
               <div className="flex flex-col gap-[5px]">
                  <img src={profile} alt="profile-img" className="rounded-[50%] object-cover w-[clamp(50px,2vmax,60px)] h-[clamp(50px,2vmax,60px)]" />
@@ -71,6 +71,47 @@ const Profile = () => {
         </aside>
 
         {/* profile jobs */}
+        <aside className="w-full flex flex-col gap-[1.4rem] md:w-[70%] p-[2rem] bg-(--white) overflow-x-auto rounded-[15px] md:h-full md:mt-0 mt-[2rem] h-[20rem] hover:border-(--light-green) border-[1px] border-(--white) transition-[border,1s,ease-in-out]">
+            <div className="flex w-full hover:translate-y-[-2px] transition-[all,1s,ease-in-out] cursor-pointer flex-col gap-[1rem] border-[1px] border-(--border-col) p-[.5rem] bg-(--light-white) rounded-[15px] hover:border-(--light-green)">
+              {/* top detail */}
+                <div className="flex w-full justify-between">
+                   <aside className="flex flex-col">
+                      <h2 className="font-bold text-[clamp(1rem,1.2vmax,1.2rem)]"> Frontend Developer </h2>
+                      <div className="flex items-center gap-[5px]">
+                         <h4 className="font-semibold text-[clamp(.8rem,1vmax,1rem)]"> Apple </h4>
+                         <p className="text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Jakarta, Indonesia </p>
+                      </div>
+                   </aside>
+
+                   <p className="text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Applied 12 day(s) ago </p>
+                </div>
+
+                <div className="flex items-center w-full justify-between">
+                   <p className="w-[70%] text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam tenetur sequi, id minima illo cum soluta itaque placeat! Id itaque nam a doloribus iusto tenetur eaque sed sequi nulla. </p>
+                   <strong className="bg-(--light-green) text-(--white) text-[clamp(.6rem,8vmax,.8rem)] p-[3px] rounded-[5px] cursor-pointer"> Read More </strong>
+                </div>
+            </div>
+
+            <div className="flex w-full hover:translate-y-[-2px] transition-[all,1s,ease-in-out] cursor-pointer flex-col gap-[1rem] border-[1px] border-(--border-col) p-[.5rem] bg-(--light-white) rounded-[15px] hover:border-(--light-green)">
+              {/* top detail */}
+                <div className="flex w-full justify-between">
+                   <aside className="flex flex-col">
+                      <h2 className="font-bold text-[clamp(1rem,1.2vmax,1.2rem)]"> Blockchain Developer </h2>
+                      <div className="flex items-center gap-[5px]">
+                         <h4 className="font-semibold text-[clamp(.8rem,1vmax,1rem)]"> Alchemy </h4>
+                         <p className="text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Boston, Usa </p>
+                      </div>
+                   </aside>
+
+                   <p className="text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Applied 12 day(s) ago </p>
+                </div>
+
+                <div className="flex items-center w-full justify-between">
+                   <p className="w-[70%] text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus veniam tenetur sequi, id minima illo cum soluta itaque placeat! Id itaque nam a doloribus iusto tenetur eaque sed sequi nulla. </p>
+                   <strong className="bg-(--light-green) text-(--white) text-[clamp(.6rem,8vmax,.8rem)] p-[3px] rounded-[5px] cursor-pointer"> Read More </strong>
+                </div>
+            </div>
+        </aside>
     </section>
   )
 }
