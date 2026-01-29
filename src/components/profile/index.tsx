c193a498f7bbd9601caff02c716a19df01789bdc
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-29 08:10:29 +0700

    completed the profile sidebar

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,24 +1,31 @@
 import { useState } from "react"
 import { Mail, Phone } from "lucide-react"
+import { profile } from "../../assets"
 
-const userDetails = [
+const details = [
   { id: 1, title: "Recent Job", value: "Apple" },
   { id: 2, title: "Last Qualification", value: "B.Tech" },
   { id: 3, title: "Work Experience", value: "4 Years" },
 ]
 
+const jobTypes = [
+  { id: 1, title: "Saved Jobs", isActive: true, total: 0 },
+  { id: 2, title: "Applied Jobs", isActive: false, total: 0 },
+]
 
 const Profile = () => {
     const [showEditBtn, setShowEditBtn] = useState(false)
+    const [userDetails, ] = useState(details)
+    const [jobTabs,] = useState(jobTypes)
 
   return (
     <section 
         className='md:w-[80%] w-full grid md:grid-cols-2 grid-cols-1 mx-auto p-[2rem] '>
         {/* profile sidebar */}
-        <aside className="flex w-[50%] md:w-[35%] flex-col sticky top-0 left-0">
-           <article className="flex flex-col gap-[1rem]">
+        <aside className="flex w-full md:w-[35%] p-[.8rem] gap-[1rem] flex-col sticky top-0 left-0">
+           <article className="w-full flex flex-col gap-[1rem]">
               <div className="flex flex-col gap-[5px]">
-                 <img src="profile-img" alt="profile-img" className="rounded-[50%] object-cover w-[clamp(40px,2vmax,50px)] h-[clamp(40px,2vmax,50px)]" />
+                 <img src={profile} alt="profile-img" className="rounded-[50%] object-cover w-[clamp(50px,2vmax,60px)] h-[clamp(50px,2vmax,60px)]" />
                  <h2 className="font-bold text-[clamp(1rem,1.2vmax,1.2rem)]"> Raazy Dev </h2>
                  <p className="text-[clamp(.8rem,1vmax,1rem)] text-(--grey)"> Jakarta, Indonesia </p>
               </div>
@@ -41,17 +48,26 @@ const Profile = () => {
               </div>
 
               {/* email & phone */}
-              <div className="w-full flex flex-col gap-[5px]">
+              <aside className="w-full flex flex-col gap-[5px]">
                   <div className="flex w-full gap-[10px] items-center">
                       <Mail className="w-[clamp(1.2rem,1.5vmax,1.5rem)] h-[clamp(1.3rem,1.5vmax,1.5rem)]" />
                       <p className="text-[clamp(.8rem,1vmax,1rem)]"> raazy.devs@gmail.com </p>
                   </div>
-              </div>
+              </aside>
+              
+              {/* buttons */}
+              <aside className="flex flex-col gap-[.6rem] w-full">
+                  <button className="px-[10px] py-[3px] rounded-[10px] w-full bg-(--light-green) font-semibold text-(--white) cursor-pointer hover:opacity-[.7] transition-[opacity,1s,ease-in-out]"> Update </button>
+                  <button className="px-[10px] py-[3px] rounded-[10px] w-full bg-(--light-green) font-semibold text-(--white) cursor-pointer hover:opacity-[.7] transition-[opacity,1s,ease-in-out]"> Upload Resume </button>
+              </aside>
 
-              <div className="flex flex-col w-full">
-                  
-              </div>
            </article>
+            {/* applied & saved jobs */}
+            <aside className="flex w-full justify-center md:flex-col flex-row gap-[.6rem] mt-[1.5rem]">
+                { jobTabs.map(type => (
+                    <h4 className={`text-[clamp(.8rem,1vmax,1rem)] hover:bg-(--light-green) cursor-pointer hover:text-(--white) rounded-[5px] p-[10px] font-semibold ${type.isActive && "bg-(--light-green) text-(--white)"}`}> {type.title} ({type.total}) </h4>
+                )) }
+            </aside>
         </aside>
 
         {/* profile jobs */}
